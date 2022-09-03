43afb7a83d51b7a17b3fe965797b26f5385ce42d
Jeffrey Rennie <surferjeff@gmail.com>
2022-09-02 22:11:09 -0700

    hello-11ty

---
+++ hello-11ty/.eleventy.js
@@ -0,0 +1,9 @@
+module.exports = function(eleventyConfig) {
+    // Return your Object options:
+    return {
+      dir: {
+        input: "11ty-src",
+        output: "src/pages"
+      }
+    }
+  };
---
+++ hello-11ty/11ty-src/README.md
@@ -0,0 +1,6 @@
+---
+layout: layout.njk
+title: A README.md
+---
+
+# Hello 11ty!
