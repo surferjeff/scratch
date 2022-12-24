fc6bfa4248cd1bf3c1b80f8b4ad997795e118c76
Jeffrey Rennie <surferjeff@gmail.com>
2022-12-24 12:22:55 -0800

    Recursive function requires rec keyword.

--- hello-fsharp-console/Program.fs
+++ hello-fsharp-console/Program.fs
@@ -4,3 +4,11 @@ printfn "Hello from F#"
 let nine = Square.square 3
 
 printfn "%A" nine
+
+let rec sumTo x =
+    if x = 0 then
+        0
+    else
+        x + (sumTo (x - 1))
+
+printfn "%A" (sumTo 5)
