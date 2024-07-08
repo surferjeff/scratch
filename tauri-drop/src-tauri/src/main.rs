c06dd386459a9e715fac99a87274db3bdcf74cec
Jeffrey Rennie <surferjeff@gmail.com>
2024-07-07 21:16:54 -0600

    Figured it out.

--- tauri-drop/src-tauri/src/main.rs
+++ tauri-drop/src-tauri/src/main.rs
@@ -1,13 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::WindowEvent;
+
 fn main() {
     tauri::Builder::default()
-        .on_page_load(|window, _payload| {
-            window.listen("tauri://file-drop-hover", |event| { dbg!(event); });
-            window.listen("tauri://file-drop", |event| { dbg!(event); });
-            window.listen("tauri://file-drop-cancelled", |event| { dbg!(event); });
-            dbg!("Listening...");
+        .on_window_event(|event| match event.event() {
+            WindowEvent::FileDrop(drop) => { dbg!(drop); },
+            _ => ()
         })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
