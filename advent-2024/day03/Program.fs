7f680e658ab76a195e561f008e4c143fc2527fd0
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-02 21:48:24 -0800

    Print dos and donts.

--- advent-2024/day03/Program.fs
+++ advent-2024/day03/Program.fs
@@ -10,3 +10,10 @@ let part1 =
         let b = int m.Groups[2].Value
         total <- total +  a * b
     printfn "part1: %d" total
+
+let part2 =
+    let text = File.ReadAllText("input.txt")
+    let rx = Regex("(?<do>do\(\))|(?<dont>don't\(\))|(?<mul>mul\(([0-9]{1,3}),([0-9]{1,3})\))")
+    for m in rx.Matches(text) do 
+        printfn "%s" (m.Groups[0].Value)
+        
