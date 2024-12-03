d299486e1a1bfbb4c8246fbabe1197a10bc9c461
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-02 21:34:14 -0800

    Find the matches.

---
+++ advent-2024/day03/Program.fs
@@ -0,0 +1,8 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+let part1 =
+    let text = File.ReadAllText("input.txt")
+    let rx = Regex("mul\([0-9]{1,3},[0-9]{1,3}\)")
+    for m in rx.EnumerateMatches(text) do
+        printfn "%s" (text.Substring(m.Index, m.Length))
