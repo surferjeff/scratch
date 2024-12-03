976d8008431e39f0979855c8c68ec485bc22b5ef
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-02 21:53:40 -0800

    part2 passes

--- advent-2024/day03/Program.fs
+++ advent-2024/day03/Program.fs
@@ -13,7 +13,18 @@ let part1 =
 
 let part2 =
     let text = File.ReadAllText("input.txt")
-    let rx = Regex("(?<do>do\(\))|(?<dont>don't\(\))|(?<mul>mul\(([0-9]{1,3}),([0-9]{1,3})\))")
+    let rx = Regex("(?<do>do\(\))|(?<dont>don't\(\))|(?<mul>mul\((?<a>[0-9]{1,3}),(?<b>[0-9]{1,3})\))")
+    let mutable doing = true
+    let mutable total = 0
     for m in rx.Matches(text) do 
         printfn "%s" (m.Groups[0].Value)
+        if m.Groups["do"].Success then
+            doing <- true
+        elif m.Groups["dont"].Success then
+            doing <- false
+        elif doing && m.Groups["a"].Success then
+            let a = int m.Groups["a"].Value
+            let b = int m.Groups["b"].Value
+            total <- total + a * b
+    printfn "part2: %d" total
         
