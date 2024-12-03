2ce2eec6492bc2806240f035a7b9549d76872af0
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-02 21:38:51 -0800

    part1 passes

--- advent-2024/day03/Program.fs
+++ advent-2024/day03/Program.fs
@@ -3,6 +3,10 @@ open System.Text.RegularExpressions
 
 let part1 =
     let text = File.ReadAllText("input.txt")
-    let rx = Regex("mul\([0-9]{1,3},[0-9]{1,3}\)")
-    for m in rx.EnumerateMatches(text) do
-        printfn "%s" (text.Substring(m.Index, m.Length))
+    let rx = Regex("mul\(([0-9]{1,3}),([0-9]{1,3})\)")
+    let mutable total = 0
+    for m in rx.Matches(text) do
+        let a = int m.Groups[1].Value
+        let b = int m.Groups[2].Value
+        total <- total +  a * b
+    printfn "part1: %d" total
