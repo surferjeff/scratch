3808fb3f7be816d35cce714fff8464e072e4ffeb
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-07 13:36:58 -0800

    day 6

--- advent-2024/day06/Program.fs
+++ advent-2024/day06/Program.fs
@@ -1,45 +1,92 @@
 ﻿open System.IO
 
-let main =
-    let lines = File.ReadAllLines("input.txt")
+let input = File.ReadAllLines("input.txt")
 
-    // Find the starting position
-    let mutable pos = (-1, -1)
-    for row in 0..lines.Length-1 do
-        if (-1, -1) = pos then
-            let line = lines[row]
-            let i = line.IndexOf('^')
-            if i >= 0 then
-                pos <- (row, i)
+// Find the starting position
+let mutable startPos = (-1, -1)
+for row in 0..input.Length-1 do
+    if (-1, -1) = startPos then
+        let line = input[row]
+        let i = line.IndexOf('^')
+        if i >= 0 then
+            startPos <- (row, i)
 
-    // Trace the path.
-    let rows = lines.Length
-    let cols = lines[0].Length
-    let lines = lines |> Array.map (fun line -> line.ToCharArray())
-    let mutable step = (-1, 0)
-    let mutable exit = false
-    while not exit do
-        let (row, col) = pos
-        lines[row][col] <-'+'
-        let (i, j) = step
-        let (row, col) = (row + i, col + j)
-        if row < 0 || row >= rows || col < 0 || col >= cols then
-            exit <- true
-        else if lines[row][col] = '#' then
-            // Turn right 90 degrees.
-            step <-
-                match step with
-                | (-1, 0) -> (0, 1)
-                | (0, 1) -> (1, 0)
-                | (1, 0) -> (0, -1)
-                | (0, -1) -> (-1, 0)
-                | bad -> failwith "Bad step"
+let rows = input.Length
+let cols = input[0].Length
+
+type WalkResult =
+| Escaped
+| InfiniteLoop
+
+// Trace the path, leaving footprints of a, b, d, and h in lines.
+// The least significant 4 bits of a, b, d, and h are distinct.
+// a: 0001
+// b: 0010
+// d: 0100
+// h: 1000
+// The most significant 4 bits are all 0110.
+let pathMask = uint8 0b01100000
+
+let walk (lines: char array array) =
+    let mutable step = (-1, 0, 'a')
+    let mutable result = None
+    let mutable (row, col) = startPos
+    while Option.isNone result do
+        let (i, j, c) = step
+
+        // Calculate the new value for this space.
+        let footPrint = lines[row][col]
+        let footPrintByte = uint8 footPrint
+        let newFootPrint =
+            if pathMask &&& footPrintByte = pathMask then
+                char (footPrintByte ||| (uint8 c))  // Merge with footprint
+            else
+                c  // Replace space with footprint.
+        if footPrint = newFootPrint then
+            result <- Some InfiniteLoop
         else
-            pos <- (row, col)
+            lines[row][col] <- newFootPrint
+
+            // Examine the next space.
+            let (nextRow, nextCol) = (row + i, col + j)
+            if nextRow < 0 || nextRow >= rows || nextCol < 0 || nextCol >= cols then
+                result <- Some Escaped
+            else if lines[nextRow][nextCol] = '#' then
+                // Turn right 90 degrees.
+                step <-
+                    match step with
+                    | (-1, 0, 'a') -> (0, 1, 'b')
+                    | (0, 1, 'b') -> (1, 0, 'd')
+                    | (1, 0, 'd') -> (0, -1, 'h')
+                    | (0, -1, 'h') -> (-1, 0, 'a')
+                    | bad -> failwith "Bad step"
+            else
+                // Leave a footprint in the next space.
+                row <- nextRow
+                col <- nextCol
+    Option.get result
+
+let part1 =
+    let lines = input |> Array.map (fun line -> line.ToCharArray())
+    walk lines |> ignore
 
     // Count path.
+    let isPathChar = fun (c: char) -> pathMask = ((uint8 c) &&& pathMask)
     let count =
         lines |> Array.sumBy (fun row -> 
-            Array.filter ((=) '+') row |> Array.length)
-    
+            Array.filter isPathChar row |> Array.length)
+
     printfn "part1 %d" count
+
+let part2 =
+    let mutable count = 0
+    for row in 0..rows-1 do
+        for col in 0..cols-1 do
+            let lines = input |> Array.map (fun line -> line.ToCharArray())
+            lines[row][col] <- '#'
+            if InfiniteLoop = walk lines then
+                count <- count + 1
+    
+    printfn "part2 %d" count
+
+
