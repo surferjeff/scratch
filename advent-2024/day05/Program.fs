e2d5d8bcd50dbc27ec113ec7c353178b2f9f0817
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-06 19:52:46 -0800

    Squashed commit of the following:
    commit b8df7d7781579c5a4cee4c4f88333964fd079136
    Author: Jeffrey Rennie <jeff@rennieglen.com>
    Date:   Fri Dec 6 16:43:04 2024 -0800
    
        Preparing for part2
    
    commit 1d7c409d08659e43930fe7db88ff6536d003df4f
    Author: Jeffrey Rennie <jeff@rennieglen.com>
    Date:   Fri Dec 6 16:30:59 2024 -0800
    
        Sets can be subtracted.
    
    commit 88d647062347e50bbdf4130ec19dd71e57323083
    Author: Jeffrey Rennie <jeff@rennieglen.com>
    Date:   Fri Dec 6 16:21:09 2024 -0800
    
        part1 solved
    
    commit f2eab213b13376a1c300d200876d2df920093a80
    Author: Jeffrey Rennie <jeff@rennieglen.com>
    Date:   Fri Dec 6 14:18:24 2024 -0800
    
        input.txt
    
    commit fecfede81b1085aabd82d5b52d9a19dfd89c654b
    Author: Jeffrey Rennie <jeff@rennieglen.com>
    Date:   Fri Dec 6 14:15:07 2024 -0800
    
        day05

---
+++ advent-2024/day05/Program.fs
@@ -0,0 +1,42 @@
+﻿open System.IO
+
+let main =
+    let lines = File.ReadAllLines("input.txt")
+    // Maps a number to a set of other numbers that must come before.
+    let mutable rules: Map<uint, Set<uint>> = Map.empty
+    let mutable sum = 0u
+    let mutable updates = []
+    for line in lines do
+        if line.Contains('|') then
+            let [|a; b|] = line.Split('|') |> Array.map (fun text -> uint text)
+            rules <- rules.Change (b, function
+                | Some set -> Some (set.Add a)
+                | None -> Some (Set.ofList [a]))
+
+        elif line.Contains(',') then
+            let update = line.Split(',') |> Array.map (fun text -> uint text)
+            updates <- update :: updates
+
+    // part 1
+    let mutable invalidUpdates = []
+    for update in updates do            
+        let mutable possibles = Set.ofSeq { 10u..99u }
+        let mutable valid = true
+        for n in update do
+            if not valid then
+                () // Nothing more todo
+            elif possibles.Contains n then
+                // This number can work.
+                match rules.TryFind n with
+                | None -> ()
+                | Some precedents -> possibles <- possibles - precedents
+            else
+                valid <- false
+        if valid then
+            sum <- sum + update[update.Length / 2]
+        else
+            invalidUpdates <- update :: invalidUpdates
+
+    printfn "part1 %d" sum
+
+    // part2
