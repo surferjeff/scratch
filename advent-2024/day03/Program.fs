4eff7e37739b4b2b7935aa7a3e75cf9a5c5808f6
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-09 23:02:20 -0800

    day03: replace while loop with Seq.fold.

--- advent-2024/day03/Program.fs
+++ advent-2024/day03/Program.fs
@@ -15,16 +15,18 @@ printfn "part1: %d" part1
 let part2 =
     let text = File.ReadAllText("input.txt")
     let rx = Regex("(?<do>do\(\))|(?<dont>don't\(\))|(?<mul>mul\((?<a>[0-9]{1,3}),(?<b>[0-9]{1,3})\))")
-    let mutable doing = true
-    let mutable total = 0
-    for m in rx.Matches(text) do 
-        if m.Groups["do"].Success then
-            doing <- true
-        elif m.Groups["dont"].Success then
-            doing <- false
-        elif doing && m.Groups["a"].Success then
-            let a = int m.Groups["a"].Value
-            let b = int m.Groups["b"].Value
-            total <- total + a * b
+    let (_, total) =
+        rx.Matches(text)
+        |> Seq.fold (fun (doing, total) m ->
+            if m.Groups["do"].Success then
+                true, total
+            elif m.Groups["dont"].Success then
+                false, total
+            elif doing then
+                true, (int m.Groups["a"].Value) * (int m.Groups["b"].Value)
+            else
+                doing, total)
+            (true, 0)
+
     printfn "part2: %d" total
         
