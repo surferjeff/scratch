1e5ef990e035f1613e94c61fbb1593313cbfa66e
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-11 11:35:28 -0800

    Switching from an F# Map to a .NET Dictionary reducted the runtime from 300 ms to 170 ms.

--- advent-2024/day11/Program.fs
+++ advent-2024/day11/Program.fs
@@ -1,18 +1,21 @@
-﻿
+﻿open System.Collections.Generic
 let input = [| 0UL; 37551UL; 469UL; 63UL; 1UL; 791606UL; 2065UL; 9983586UL |]
 
 // Maps the number engraved on the stone to the number of times it appears in
 // the line.
-type StoneMap = Map<uint64,uint64>
+// Switching from an F# Map to a .NET Dictionary reducted the runtime from
+// 300 ms to 219 ms.
+type StoneMap = Dictionary<uint64,uint64>
 
-let addStone (stone: uint64) (count: uint64) (stones: StoneMap) =
-    stones |> Map.change stone (function
-    | None -> Some count
-    | Some n -> Some (n + count))
+let addStone stone count (stones: StoneMap) =
+    match stones.TryGetValue(stone) with
+    | true, vcount -> stones[stone] <- vcount + count
+    | false, _ -> stones[stone] <- count
+    stones
 
 let blink (stones: StoneMap) _ =
     stones
-    |> Map.toSeq
+    |> Seq.map (fun kv -> (kv.Key, kv.Value))
     |> Seq.fold (fun newStones (stone, count) ->
         if stone = 0UL then
             newStones |> addStone 1UL count
@@ -25,14 +28,14 @@ let blink (stones: StoneMap) _ =
                 |> addStone (uint64 (text.Substring(halfLength))) count
             else
                 newStones |> addStone (stone * 2024UL) count
-    ) Map.empty
+    ) (StoneMap(stones.Count * 2))
 
 let part1 =
-    let stones = input |> Seq.map (fun n -> (n, 1UL)) |> Map
+    let stones = input |> Seq.map (fun n -> KeyValuePair(n, 1UL)) |> StoneMap
     Seq.fold blink stones { 1..25 }
 
-printfn "part1: %d" (part1 |> Map.values |> Seq.sum)
+printfn "part1: %d" (part1.Values |> Seq.sum)
 
 let part2 = Seq.fold blink part1 { 1..50 }
 
-printfn "part2: %d" (part2 |> Map.values |> Seq.sum)
+printfn "part2: %d" (part2.Values |> Seq.sum)
