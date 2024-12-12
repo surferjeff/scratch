7c7a27a8f179940b8bff695e2d0b162fd359d70b
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-12 09:06:46 -0800

    part1 correct answer

--- advent-2024/day12/Program.fs
+++ advent-2024/day12/Program.fs
@@ -1,15 +1,13 @@
 ﻿open System.IO
 
-let input = File.ReadAllLines("input.txt")
-
 // A capital letter represents unvisited.  Lowercase means visited.
-// # means beyond the edge.
+// ~ means beyond the edge.
 let inputToGrid (lines: string array) =
     let cols = lines[0].Length
-    let border = [| String.replicate (cols + 2) "#" |]
+    let border = [| String.replicate (cols + 2) "~" |]
     let grid = Seq.concat [
         border
-        lines |> Array.map (fun line -> $"#{line}#")
+        lines |> Array.map (fun line -> $"~{line}~")
         border
     ]
     array2D grid
@@ -17,18 +15,41 @@ let inputToGrid (lines: string array) =
 let toUpper (c: char) = char ((int c) &&& 0b11011111)
 let toLower (c: char) = char ((int c) ||| 0b00100000)
 
+let allGridCoords arr = seq {
+    for i in 0..Array2D.length1 arr - 1 do
+        for j in 0..Array2D.length2 arr - 1 do
+            yield (i, j)
+}
+
 let rec flood (grid: char[,]) i j =
     let c = Array2D.get grid i j
-    Array2D.set grid i j (toLower c)
-    [ (i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1)]
-    |> List.fold (fun (area, fence) (ni, nj) ->
-        let nc = Array2D.get grid ni nj
-        if nc = '#' then // boundary
-            (area, fence + 1)
-        elif c = nc then // unvisited matching color
-            flood grid ni nj
-        elif c = (toUpper nc) then // already visited matching color
-            (area, fence)
-        else // mismatched colors
-            (area, fence + 1)
-    ) (1, 0)
+    if c >= 'a' then
+        (0, 0)  // already visited or boundary
+    else
+        Array2D.set grid i j (toLower c)
+        [ (i + 1, j); (i - 1, j); (i, j + 1); (i, j - 1)]
+        |> List.fold (fun (area, fence) (ni, nj) ->
+            let nc = Array2D.get grid ni nj
+            if c = nc then // unvisited matching color
+                let (narea, nfence) = flood grid ni nj
+                (area + narea, fence + nfence)
+            elif c = (toUpper nc) then // already visited matching color
+                (area, fence)
+            else // mismatched colors; erect a fence.
+                (area, fence + 1)
+        ) (1, 0)
+
+let part1 =
+    let grid =
+        File.ReadAllLines("input.txt")
+        |> inputToGrid
+    allGridCoords grid
+    |> Seq.map (fun (i, j) ->
+        let (area, fence) = flood grid i j
+        area * fence)
+    |> Seq.sum
+
+printf "part1: %d" part1
+    
+
+
