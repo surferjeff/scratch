6479fc807aac37c8f36b15d9465e17a7e50dd9a0
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-12 22:40:22 -0800

    Parse the input.

--- advent-2024/day13/Program.fs
+++ advent-2024/day13/Program.fs
@@ -1,2 +1,35 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System.IO
+open System.Text.RegularExpressions
+
+[<Struct>]
+type Game = {
+    A: int * int
+    B: int * int
+    Prize: int * int
+}
+
+let zeroGame = { A = (0, 0); B = (0, 0); Prize = (0, 0) }
+
+let readInput path =
+    let rx = Regex("(Button A: X\+(?<AX>\d+), Y\+(?<AY>\d+))"
+        + "|(Button B: X\+(?<BX>\d+), Y\+(?<BY>\d+))"
+        + "|(Prize: X=(?<ZX>\d+), Y=(?<ZY>\d+))")
+    let getXY (m: Match) (x: string) (y: string) =
+        (int m.Groups[x].Value, int m.Groups[y].Value)
+    let _, games =
+        rx.Matches(File.ReadAllText path)
+        |> Seq.fold (fun (game, accGames) m ->
+            if m.Groups["AX"].Success then
+                ({game with A = getXY m "AX" "AY"}, accGames)
+            elif m.Groups["BX"].Success then
+                ({game with B = getXY m "BX" "BY"}, accGames)
+            elif m.Groups["ZX"].Success then
+                (zeroGame, {game with Prize = getXY m "ZX" "ZY"} :: accGames)
+            else
+                (game, accGames)
+        ) (zeroGame, [])
+    games
+
+let games = readInput "input.txt"
+for game in games do
+    printfn "%A" game
