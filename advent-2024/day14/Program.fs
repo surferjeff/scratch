5f3ad451a8c7641b9c98fcf362885855aa7ece47
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-14 11:02:14 -0800

    parseInput

---
+++ advent-2024/day14/Program.fs
@@ -0,0 +1,24 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+[<Struct>]
+type Robot = {
+    // Position
+    pX: int; pY: int
+    // Velocity
+    vX: int; vY: int
+}
+
+let parseInput path =
+    let text = File.ReadAllText(path)
+    let rx = Regex("p=(?<pX>-?\\d+),(?<pY>-?\\d+) v=(?<vX>-?\\d+),(?<vY>-?\\d+)")
+    rx.Matches(text)
+    |> Seq.map (fun m -> {
+        pX = int m.Groups["pX"].Value
+        pY = int m.Groups["pY"].Value
+        vX = int m.Groups["vX"].Value
+        vY = int m.Groups["vY"].Value
+    })
+    |> Seq.toList
+
+printfn "%A" (parseInput "test1.txt")
