29126d3dd1e20bfd0302fcad7253857932a79f7f
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-17 08:40:38 -0800

    parseInput

---
+++ advent-2024/day17/Program.fs
@@ -0,0 +1,45 @@
+﻿open System.IO
+open System.Text.RegularExpressions
+
+[<Struct>]
+type Registers = {
+    A: int64
+    B: int64
+    C: int64
+}
+let zeroRegisters = { A = 0L; B = 0L; C = 0L}
+
+[<Struct>]
+type Operation = {
+    Code: int
+    Operand: int
+}
+
+let parseInput path =
+    let rx = Regex("(Register (?<regname>A|B|C): (?<regvalue>\\d+))"
+        + "|((?<opcode>\\d+),(?<operand>\\d+))")
+    let mutable regs = zeroRegisters
+    let ops = ResizeArray<Operation>()
+    for m in File.ReadAllText path |> rx.Matches do
+        if m.Groups["regname"].Success then
+            let regValue = int64 m.Groups["regvalue"].Value
+            match m.Groups["regname"].Value with
+            | "A" -> regs <- { regs with A = regValue }
+            | "B" -> regs <- { regs with B = regValue }
+            | "C" -> regs <- { regs with C = regValue }
+            | bad -> failwithf "Bad register name %s" bad
+        else
+            {
+                Code = int m.Groups["opcode"].Value
+                Operand = int m.Groups["operand"].Value
+            } |> ops.Add        
+
+    regs, ops
+
+let execute (regs: Registers) (op: Operation): Registers =
+    zeroRegisters
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" (parseInput argv[0])
+    0
