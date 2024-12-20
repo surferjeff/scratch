96cabaf0bb832eba6b76f17d52519fa9bc70fe55
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-20 13:53:13 -0800

    day20 part 1

--- advent-2024/Notes.md
+++ advent-2024/Notes.md
@@ -1,9 +1,48 @@
 
 
-See 072c9f9f797e9608488afba3f2fbae4e19e4d301
+Day 16 072c9f9f797e9608488afba3f2fbae4e19e4d301
 
 First I implemented a loop with a tail recursion and got a stack overflow.
 So I learn Seq.unfold.  It worked, but was slow and the code was hard
 to understand.  So, I switched to a while loop and mutable code and
 everything improved.  Add 'continue' and 'return' statements to F#, and the
 code would just about be perfect.
+
+
+Day 20 c2e9dc97e751ffcef0d1f2f5271516fb5a735f9f
+
+There's a bug in this code:
+```
+    paths
+    |> enumArray2D
+    |> Seq.choose (fun (row, col, square) ->
+        match square with
+        | None -> None
+        | Some start -> Some (row, col, start))
+    |> Seq.map (fun (startRow, startCol, cheatStart) ->
+        cheatDirs
+        |> Seq.map (fun (dRow, dCol) -> startRow + dRow, startCol + dCol)
+        |> Seq.filter inBounds
+        |> Seq.choose (fun (row, col) -> paths[row, col])
+        |> Seq.map (fun cheatEnd -> cheatEnd - cheatStart)
+        |> Seq.filter (fun cheatSavings -> cheatSavings > 1))
+    |> Seq.collect id
+    |> Seq.groupBy id
+    |> Seq.map (fun (n, nlist) -> (n, Seq.length nlist))
+    |> Seq.sort
+    |> Seq.toList
+```
+
+I initially started writing this code with if statements and while loops,
+but the inner code was indented more than halfway across the screen.
+`continue`, `break`, and `return` statements would have kept the indentation
+reasonable, but they're missing from F#.
+
+So, I switched to F#'s `filter`, `map`, `c`, etc.  Problems:
+1. It was very difficult to write, even after 20 days of intensely working 
+   through Advent of Code problems with F#.  I had lots of syntax errors.
+   For puzzles, I guess it's OK.  When I'm trying to solve users' problems,
+   I don't want to waste energy trying to solve F#'s problems.
+2. Debugging this code is very hard.  Inserting `printfn` statements isn't
+   simple.  Neither is setting breakpoints in useful places.
+
---
+++ advent-2024/day20/Program.fs
@@ -0,0 +1,75 @@
+﻿open System.IO
+open System.Collections.Generic
+
+let enumArray2D arr =
+    seq {
+        for i in 0..(Array2D.length1 arr - 1) do
+            for j in 0..(Array2D.length2 arr - 1) do
+                yield i, j, arr[i, j]
+    }
+
+let race (maze: string array) =
+    // Find the start position.
+    let mutable startRow, startCol = -1, -1
+    for iRow in 0..maze.Length-1 do
+        let iCol = maze[iRow].IndexOf('S')
+        if iCol >= 0 then
+            startRow <- iRow
+            startCol <- iCol
+
+    // Create a 2d array to store the path.
+    let rows = maze.Length
+    let cols = maze[0].Length
+    let paths = Array2D.create rows cols None
+    paths[startRow, startCol] <- Some(0)
+
+    // Explore, leaving shortest path to each square in paths.
+    let dirs = [ 1, 0; -1, 0; 0, 1; 0, -1]
+    let q = Queue([startRow, startCol, 1])
+    while q.Count > 0 do
+        let proRow, proCol, stepCount = q.Dequeue()
+        for dRow, dCol in dirs do
+            let stepRow, stepCol = proRow + dRow, proCol + dCol
+            match maze[stepRow][stepCol], paths[stepRow, stepCol] with
+            | ('.' | 'E'), None ->
+                paths[stepRow, stepCol] <- Some stepCount
+                q.Enqueue(stepRow, stepCol, stepCount + 1)
+            | _ -> ()
+
+    // Find cheats.
+    let inBounds (row, col) = row >= 0 && row < rows && col >= 0 && col < cols
+    let cheatDirs = dirs |> List.map (fun (i, j) -> i * 2, j * 2)
+    paths
+    |> enumArray2D
+    |> Seq.choose (fun (row, col, square) ->
+        match square with
+        | None -> None
+        | Some start -> Some (row, col, start))
+    |> Seq.toList
+    |> List.map (fun (startRow, startCol, cheatStart) ->
+        cheatDirs
+        |> List.map (fun (dRow, dCol) -> startRow + dRow, startCol + dCol)
+        |> List.filter inBounds
+        |> List.choose (fun (row, col) -> paths[row, col])
+        |> List.map (fun cheatEnd -> cheatEnd - cheatStart - 2)
+        |> List.filter(fun cheatSavings -> cheatSavings > 0))
+    |> List.collect id
+    |> List.groupBy id
+    |> List.map (fun (n, nlist) -> (n, List.length nlist))
+    |> List.sort
+
+
+[<EntryPoint>]
+let main argv =
+    let cheats = argv[0] |> File.ReadAllLines |> race
+    printfn "%A" cheats
+    cheats
+    |> List.map (fun (savings, count) ->
+        if savings >= 100 then
+            count
+        else
+            0)
+    |> List.sum
+    |> printfn "part1: %d"
+    0
+    
