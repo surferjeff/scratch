e91dfbf34c86ab061c2dd74b875b4f237e768690
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-21 11:57:49 -0800

    Counting won't be enough.

--- advent-2024/day21/Program.fs
+++ advent-2024/day21/Program.fs
@@ -22,17 +22,26 @@ let mapFromPad (pad: string list) =
 let numberMap = mapFromPad numberPad
 let arrowMap = mapFromPad arrowPad
 
-let countMoves map keyStart keyEnd =
+let pipePrint format thing =
+    printfn format thing
+    thing
+
+let countMoves map (keyStart, keyEnd) =
     let rowStart, colStart = Map.find keyStart map
     let rowEnd, colEnd = Map.find keyEnd map
-    abs (rowStart - rowEnd) + abs (colStart - colEnd)
+    1 + abs (rowStart - rowEnd) + abs (colStart - colEnd)
 
 let countMovesInPattern (map: Map<char, int*int>) (pattern: char array) =
     let mutable moves = []
     for i in pattern.Length-2..-1..0 do
         moves <- (pattern[i], pattern[i+1]) :: moves
     moves <- ('A', pattern[0]) :: moves
-    printfn "%A" moves
+    moves
+    |> pipePrint "%A"
+    |> List.map (countMoves map)
+    |> pipePrint "%A"
+    |> List.sum
 
 countMovesInPattern numberMap ("029A".ToCharArray())
+|> printfn "%d"
 
