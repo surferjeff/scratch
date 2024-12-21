73a9b3ceb9875339a555b893f9c0d26f24131965
Jeffrey Rennie <jeff@rennieglen.com>
2024-12-21 12:19:12 -0800

    movesFromMap

--- advent-2024/day21/Program.fs
+++ advent-2024/day21/Program.fs
@@ -16,32 +16,57 @@ let mapFromPad (pad: string list) =
     for row in 0..pad.Length-1 do
         let line = pad[row]
         for col in 0..line.Length - 1 do
-            grid <- Map.add line[col] (row, col) grid
+            let c = line[col]
+            if c <> ' ' then
+                grid <- Map.add c (row, col) grid
     grid
 
-let numberMap = mapFromPad numberPad
-let arrowMap = mapFromPad arrowPad
+let movesFromMap (map: Map<char, int*int>) =
+    Seq.allPairs (Map.keys map) (Map.keys map)
+    |> Seq.map (fun (keyStart, keyEnd) -> 
+        let rowStart, colStart = Map.find keyStart map
+        let rowEnd, colEnd = Map.find keyEnd map
+        let vertMove =
+            match rowEnd - rowStart with
+            | 0 -> ""
+            | n when n < 0 -> String.replicate -n "^"
+            | n -> String.replicate n "v"
+        let horizMove =
+            match colEnd - colStart with
+            | 0 -> ""
+            | n when n < 0 -> String.replicate -n "<"
+            | n -> String.replicate n ">"
+        ((keyStart, keyEnd), vertMove + horizMove))
+    |> Map
+
+let movesFromPad = mapFromPad >> movesFromMap
+
+let numberMoves = movesFromPad numberPad
+let arrowMoves = movesFromPad arrowPad
+
+printfn "%A" numberMoves
+
 
 let pipePrint format thing =
     printfn format thing
     thing
 
-let countMoves map (keyStart, keyEnd) =
-    let rowStart, colStart = Map.find keyStart map
-    let rowEnd, colEnd = Map.find keyEnd map
-    1 + abs (rowStart - rowEnd) + abs (colStart - colEnd)
+// let countMoves map (keyStart, keyEnd) =
+//     let rowStart, colStart = Map.find keyStart map
+//     let rowEnd, colEnd = Map.find keyEnd map
+//     1 + abs (rowStart - rowEnd) + abs (colStart - colEnd)
 
-let countMovesInPattern (map: Map<char, int*int>) (pattern: char array) =
-    let mutable moves = []
-    for i in pattern.Length-2..-1..0 do
-        moves <- (pattern[i], pattern[i+1]) :: moves
-    moves <- ('A', pattern[0]) :: moves
-    moves
-    |> pipePrint "%A"
-    |> List.map (countMoves map)
-    |> pipePrint "%A"
-    |> List.sum
+// let countMovesInPattern (map: Map<char, int*int>) (pattern: char array) =
+//     let mutable moves = []
+//     for i in pattern.Length-2..-1..0 do
+//         moves <- (pattern[i], pattern[i+1]) :: moves
+//     moves <- ('A', pattern[0]) :: moves
+//     moves
+//     |> pipePrint "%A"
+//     |> List.map (countMoves map)
+//     |> pipePrint "%A"
+//     |> List.sum
 
-countMovesInPattern numberMap ("029A".ToCharArray())
-|> printfn "%d"
+// countMovesInPattern numberMap ("029A".ToCharArray())
+// |> printfn "%d"
 
