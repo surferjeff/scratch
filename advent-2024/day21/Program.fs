83bdffbd23362bd81874d2aac1b170fcf14a5757
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-22 21:15:55 -0800

    movesFromMap

--- advent-2024/day21/Program.fs
+++ advent-2024/day21/Program.fs
@@ -12,6 +12,10 @@ let arrowPad = [
     "<v>"
 ]
 
+let pipePrint format thing =
+    printfn format thing
+    thing
+
 let mapFromPad (pad: string list) =
     let mutable grid = Map.empty
     for row in 0..pad.Length-1 do
@@ -47,7 +51,8 @@ type MovesMap = Dictionary<char*char*char option, string>
 let movesFromMap (map: Map<char, int*int>) =
     let moveMap = MovesMap()
     let moveIsLegal = moveIsLegal (Map.find ' ' map)
-    for (keyStart, keyEnd) in Seq.allPairs (Map.keys map) (Map.keys map) do
+    let legalKeys = map.Keys |> Seq.filter (fun c -> c <> ' ') |> Seq.toList
+    for (keyStart, keyEnd) in Seq.allPairs legalKeys legalKeys  do
         let rowStart, colStart = Map.find keyStart map
         let rowEnd, colEnd = Map.find keyEnd map
         let vertMoves = enumMoves "^" "v" rowEnd rowStart
@@ -55,57 +60,55 @@ let movesFromMap (map: Map<char, int*int>) =
         if vertMoves.Length > 0 || horizMoves.Length > 0 then
             let legalMoves = 
                 [vertMoves + horizMoves; horizMoves + vertMoves]
+                |> List.distinct
                 |> List.filter (moveIsLegal rowStart colStart)
             for moves in legalMoves do
-                moveMap[(keyStart, keyEnd, Some moves[0])] <- moves
-            moveMap[(keyStart, keyEnd, None)] <- legalMoves[0]
+                moveMap.Add((keyStart, keyEnd, Some moves[0]), moves)
+            moveMap.Add((keyStart, keyEnd, None), legalMoves[0])
     moveMap
-
 
 let movesFromPad = mapFromPad >> movesFromMap
 let numberMoves = movesFromPad numberPad
 let arrowMoves = movesFromPad arrowPad
 
-let pipePrint format thing =
-    printfn format thing
-    thing
+numberMoves |> Seq.toList |> printfn "%A" 
 
-let enumMovesInPattern (movesMap: MovesMap) (pattern: string) =
-    let mutable moves = []
-    for i in pattern.Length-2..-1..0 do
-        moves <- (pattern[i], pattern[i+1]) :: moves
-    moves <- ('A', pattern[0]) :: moves
-    moves |> List.map (fun a2b -> (Map.find a2b movesMap) + "A")
+// let enumMovesInPattern (movesMap: MovesMap) (pattern: string) =
+//     let mutable moves = []
+//     for i in pattern.Length-2..-1..0 do
+//         moves <- (pattern[i], pattern[i+1]) :: moves
+//     moves <- ('A', pattern[0]) :: moves
+//     moves |> List.map (fun a2b -> (Map.find a2b movesMap) + "A")
 
-let tripleCode (code: string) =
-    code
-    |> pipePrint "%A"
-    |> enumMovesInPattern numberMoves
-    |> pipePrint "%A"
-    |> String.concat ""
-    |> enumMovesInPattern arrowMoves
-    |> pipePrint "%A"
-    |> String.concat ""
-    |> enumMovesInPattern arrowMoves
-    |> pipePrint "%A"
-    |> String.concat ""
+// let tripleCode (code: string) =
+//     code
+//     |> pipePrint "%A"
+//     |> enumMovesInPattern numberMoves
+//     |> pipePrint "%A"
+//     |> String.concat ""
+//     |> enumMovesInPattern arrowMoves
+//     |> pipePrint "%A"
+//     |> String.concat ""
+//     |> enumMovesInPattern arrowMoves
+//     |> pipePrint "%A"
+//     |> String.concat ""
 
-let part1 (codes: string list) =
-    codes
-    |> List.map (fun code -> code, (tripleCode code))
-    |> List.map (fun (code, tripleCode) -> (tripleCode.Length, int code[0..code.Length-2]))
-    |> pipePrint "%A"
-    |> List.map (fun (n, m) -> n * m)
-    |> List.sum
-    |> printfn "part1: %d"
+// let part1 (codes: string list) =
+//     codes
+//     |> List.map (fun code -> code, (tripleCode code))
+//     |> List.map (fun (code, tripleCode) -> (tripleCode.Length, int code[0..code.Length-2]))
+//     |> pipePrint "%A"
+//     |> List.map (fun (n, m) -> n * m)
+//     |> List.sum
+//     |> printfn "part1: %d"
 
-[<EntryPoint>]
-let main argv =
-    part1 [
-        // "029A"
-        // "980A"
-        // "179A"
-        // "456A"
-        "379A"
-    ]
-    0
+// [<EntryPoint>]
+// let main argv =
+//     part1 [
+//         // "029A"
+//         // "980A"
+//         // "179A"
+//         // "456A"
+//         "379A"
+//     ]
+//     0
