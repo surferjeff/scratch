0d2309b1049362f660d2e248b29921493bc53dd4
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-23 22:24:52 -0800

    Debugging.

--- advent-2024/day23/Program.fs
+++ advent-2024/day23/Program.fs
@@ -1,16 +1,18 @@
 ﻿open System.IO
 
 let addLink (sets: Map<string, int>) compA compB setNo =
+    printfn "%A" sets
     match Map.tryFind compA sets, Map.tryFind compB sets with
     | None, None -> sets |> Map.add compA setNo |> Map.add compB setNo
     | Some(n), None -> sets |> Map.add compB n
     | None, Some(n) -> sets |> Map.add compA n
+    | Some(n), Some(m) when n = m -> sets
     | Some(n), Some(m) ->
         // Merge the two sets
         sets
-        |> Map.toSeq
-        |> Seq.choose (fun (key, valu) -> if valu = n then Some(key) else None)
-        |> Seq.fold (fun sets comp -> Map.add comp m sets) sets
+        |> Map.toList
+        |> List.choose (fun (key, valu) -> if valu = n then Some(key) else None)
+        |> List.fold (fun sets comp -> Map.add comp m sets) sets
 
 [<EntryPoint>]
 let main argv =
