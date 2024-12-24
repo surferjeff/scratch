797ff6d4e027d238911c4f520167b26205610f66
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-24 15:03:16 -0800

    propagate

--- advent-2024/day24/Program.fs
+++ advent-2024/day24/Program.fs
@@ -1,25 +1,50 @@
 ﻿open System.IO
 
+[<Struct>]
 type Gate = {
     Wires: string list
     Signals: int list
     Op: string
 }
 
+// Checks to see if gate's input wires are carrying a signal.  If so, updates
+// allWires with its signal and returns None.
+let propagate (allWires: Map<string, int>) (gateName: string, gate: Gate) =
+    let wires, signals =
+        gate.Wires
+        |> List.fold (fun (gateWires, signals) wire ->
+            match Map.tryFind wire allWires with
+            | None -> (wire :: gateWires, signals)
+            | Some signal -> (gateWires, signal :: signals)) (List.empty, List.empty)
+    if List.isEmpty wires then
+        let signal =
+            match gate.Op with
+            | "AND" -> List.fold (&&&) 1 gate.Signals
+            | "OR" -> List.fold (|||) 0 gate.Signals
+            | "XOR" -> List.fold (^^^) 0 gate.Signals
+            | bad -> failwithf "Bad operation %s" bad
+        Map.add gateName signal allWires, None
+    else
+        allWires, Some {gate with Wires = wires; Signals = signals}
+
+
 [<EntryPoint>]
 let main argv =
-    File.ReadAllLines argv[0]
-    |> Array.fold (fun (wires, gates) line ->
-        if line.Contains(':') then
-            let words = line.Split(": ") |> Array.map (fun s -> s.Trim())
-            let [| wire;  value |] = words
-            (wire, value) :: wires, gates
-        elif line.Contains("->") then
-            let words = line.Split() |> Array.map (fun s -> s.Trim())
-            let [| wireA; op; wireB; _; wireC |] = words
-            (wires, (wireC, { Wires = [wireA; wireB]; Signals = []; Op = op}) :: gates)
-        else
-            (wires, gates)
-    ) (List.empty, List.empty)
-    |> printfn "%A"
+    let wires, gates =
+        File.ReadAllLines argv[0]
+        |> Array.fold (fun (wires, gates) line ->
+            if line.Contains(':') then
+                let words = line.Split(": ") |> Array.map (fun s -> s.Trim())
+                let [| wire;  value |] = words
+                Map.add wire (int value) wires,  gates
+            elif line.Contains("->") then
+                let words = line.Split() |> Array.map (fun s -> s.Trim())
+                let [| wireA; op; wireB; _; wireC |] = words
+                (wires, (wireC, { Wires = [wireA; wireB]; Signals = []; Op = op}) :: gates)
+            else
+                (wires, gates)
+        ) (Map.empty, List.empty)
+
+    propagate wires (List.head gates) |> printfn "%A"
+
     0
