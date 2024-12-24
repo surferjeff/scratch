e679fbc86529817abe590b2174cb81a6b47928ec
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-24 10:49:24 -0800

    day2 is wrong

--- advent-2024/day23/Program.fs
+++ advent-2024/day23/Program.fs
@@ -28,13 +28,51 @@ let addEdge (graph: Graph) (edge: string array) =
         | Some edges -> Some (Set.add edge[0] edges)
         | None -> Some (Set.ofList [edge[0]]))
 
+
+let addEdge2 (sets: Map<string, int>) compA compB setNo =
+    match Map.tryFind compA sets, Map.tryFind compB sets with
+    | None, None -> sets |> Map.add compA setNo |> Map.add compB setNo
+    | Some(n), None -> sets |> Map.add compB n
+    | None, Some(n) -> sets |> Map.add compA n
+    | Some(n), Some(m) when n = m -> sets
+    | Some(n), Some(m) ->
+        // Merge the two sets
+        sets
+        |> Map.toList
+        |> List.choose (fun (key, valu) -> if valu = n then Some(key) else None)
+        |> List.fold (fun sets comp -> Map.add comp m sets) sets
+
 [<EntryPoint>]
 let main argv =
-    File.ReadAllLines argv[0]
-    |> Array.map (fun (line: string) -> line.Split('-'))
+    let edges = 
+        File.ReadAllLines argv[0]
+        |> Array.map (fun (line: string) -> line.Split('-'))
+
+    edges
     |> Array.fold addEdge Map.empty
     |> findTriangles
     |> Seq.filter (List.exists (fun node -> node.StartsWith('t')))
     |> Seq.length
     |> printfn "part1: %d"
+
+    let sets =
+        edges
+        |> Array.fold (fun (sets, i) comps ->
+            addEdge2 sets comps[0] comps[1] i, i + 1 ) (Map.empty, 0)
+        |> fst
+    let largestGroupId =
+        sets
+        |> Map.values
+        |> Seq.groupBy id
+        |> Seq.map (fun (setId, setIds) -> setId, Seq.length setIds )
+        |> Seq.maxBy (fun (groupId, count) -> count)
+        |> fst
+    sets
+    |> Map.toList
+    |> List.filter (fun (comp, group) -> group = largestGroupId)
+    |> List.map fst
+    |> List.sort
+    |> String.concat ","
+    |> printfn "%s"
+
     0
