2b7d036a0eff06d6ff1c70768500c550c692353a
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-23 22:13:24 -0800

    Everything ends up in set 0.

---
+++ advent-2024/day23/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.IO
+
+let addLink (sets: Map<string, int>) compA compB setNo =
+    match Map.tryFind compA sets, Map.tryFind compB sets with
+    | None, None -> sets |> Map.add compA setNo |> Map.add compB setNo
+    | Some(n), None -> sets |> Map.add compB n
+    | None, Some(n) -> sets |> Map.add compA n
+    | Some(n), Some(m) ->
+        // Merge the two sets
+        sets
+        |> Map.toSeq
+        |> Seq.choose (fun (key, valu) -> if valu = n then Some(key) else None)
+        |> Seq.fold (fun sets comp -> Map.add comp m sets) sets
+
+[<EntryPoint>]
+let main argv =
+    File.ReadAllLines argv[0]
+    |> Array.map (fun (line: string) -> line.Split('-'))
+    |> Array.fold (fun (sets, i) comps ->
+        addLink sets comps[0] comps[1] i, i + 1 ) (Map.empty, 0)
+    |> fst
+    |> Map.iter (fun comp set -> printfn "%s %d" comp set)
+    0
