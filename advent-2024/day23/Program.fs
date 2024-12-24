b7b8ef6202b6361b4093bb0f49d160dfbf89f56a
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-23 23:40:07 -0800

    findTriangles

--- advent-2024/day23/Program.fs
+++ advent-2024/day23/Program.fs
@@ -1,25 +1,47 @@
-﻿open System.IO
+﻿open System
+open System.IO
 
-let addLink (sets: Map<string, int>) compA compB setNo =
-    printfn "%A" sets
-    match Map.tryFind compA sets, Map.tryFind compB sets with
-    | None, None -> sets |> Map.add compA setNo |> Map.add compB setNo
-    | Some(n), None -> sets |> Map.add compB n
-    | None, Some(n) -> sets |> Map.add compA n
-    | Some(n), Some(m) when n = m -> sets
-    | Some(n), Some(m) ->
-        // Merge the two sets
-        sets
-        |> Map.toList
-        |> List.choose (fun (key, valu) -> if valu = n then Some(key) else None)
-        |> List.fold (fun sets comp -> Map.add comp m sets) sets
+type Graph = Map<string, Set<string>>
+
+// Function to find triangles in an undirected graph using hash-based edge iterator
+let findTriangles (graph: Graph) =
+    let triangles = ResizeArray<(string * string * string)>()
+
+    // Iterate through each node in the graph
+    for u in graph.Keys do
+        // Get neighbors of 'u'
+        let neighborsU = graph.[u]
+
+        for v in neighborsU do
+            // Ensure u < v to avoid duplicate pairs
+            if String.Compare(u, v) < 0 then
+                let neighborsV = graph.[v]
+
+                // Find the intersection of neighbors of 'u' and 'v'
+                let commonNeighbors = Set.intersect neighborsU neighborsV
+
+                for w in commonNeighbors do
+                    // Ensure v < w to avoid duplicate triangles
+                    if String.Compare(v, w) < 0 then
+                        triangles.Add((u, v, w))
+
+    // Return the list of triangles
+    triangles
+
+let addEdge (graph: Graph) (edge: string array) =
+    graph
+    |> Map.change edge[0] (function
+        | Some edges -> Some (Set.add edge[1] edges)
+        | None -> Some (Set.ofList [edge[1]]))
+    |> Map.change edge[1] (function
+        | Some edges -> Some (Set.add edge[0] edges)
+        | None -> Some (Set.ofList [edge[0]]))
 
 [<EntryPoint>]
 let main argv =
     File.ReadAllLines argv[0]
     |> Array.map (fun (line: string) -> line.Split('-'))
-    |> Array.fold (fun (sets, i) comps ->
-        addLink sets comps[0] comps[1] i, i + 1 ) (Map.empty, 0)
-    |> fst
-    |> Map.iter (fun comp set -> printfn "%s %d" comp set)
+    |> Array.fold addEdge Map.empty
+    |> findTriangles
+    |> Seq.iter (printfn "%A")
     0
