bbb6ddf1a067ffa9adf9a2bb0137f78c755ba854
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-27 12:01:40 -0800

    Region with # borders.

---
+++ advent-2024/day18/Program.fs
@@ -0,0 +1,13 @@
+﻿
+[<EntryPoint>]
+let main argv =
+    let dim = 6
+    let topBottom = String.replicate (dim + 3) "#"
+    let middle = "#" + (String.replicate (dim + 1) ".") + "#"
+    let region =
+        {0..dim}
+        |> Seq.fold (fun lines _ -> middle :: lines) [topBottom]
+        |> List.append [topBottom]
+        |> List.toArray
+        |> Array.iter (printfn "%s")
+    0
