a22a2298b1b4447900b937f6f5b1676e1b0da277
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-27 15:03:55 -0800

    Squashed commit of the following:
    commit fc9d1d696a612aeebb2d321da18b8d6efe236467
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 15:03:06 2024 -0800
    
        Remove printing statements.
    
    commit 334437af5e29e153d9381880882e0c00182c1dd9
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 15:01:35 2024 -0800
    
        part2 correct!
    
    commit c42db65036eb61c258b3ad52b86d79c4e01d9d25
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 14:48:26 2024 -0800
    
        split byteDrops
    
    commit 8ce667551657c32324652af9b8a95f70f709ab9b
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 14:09:07 2024 -0800
    
        findPath function
    
    commit 0bb3f9cc8f35f8388285fecdd1111d3d3f8c2495
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 14:01:47 2024 -0800
    
        part1 function
    
    commit 931a4c53dd4f069389680622d3a0a353ebd3dd6f
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 13:52:07 2024 -0800
    
        part1  correct answer
    
    commit bf4b5eece96d6a3c39db851dc24c0ca93310b2b7
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 13:48:54 2024 -0800
    
        Find the shortest path.
    
    commit 5987999191a5bee6a0ee3cd2777344bcd2568d94
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 12:23:31 2024 -0800
    
        Print the region.
    
    commit bbb6ddf1a067ffa9adf9a2bb0137f78c755ba854
    Author: Jeffrey Rennie <surferjeff@gmail.com>
    Date:   Fri Dec 27 12:01:40 2024 -0800
    
        Region with # borders.

---
+++ advent-2024/day18/Program.fs
@@ -0,0 +1,60 @@
+﻿open System.IO
+open System.Collections.Generic
+
+let findPath (region: char array2d) =
+    let dim = Array2D.length1 region - 2
+    let q = Queue([1, 1, []])
+    let mutable found = []
+    while q.Count > 0 do
+        let (row, col, tail) = q.Dequeue()
+        if row = dim && col = dim then
+            found <- (row, col) :: tail
+            q.Clear()
+        elif region[row, col] = '.' then
+            region[row, col] <- ','
+            let tail = (row, col) :: tail
+            q.Enqueue(row + 1, col, tail)
+            q.Enqueue(row - 1, col, tail)
+            q.Enqueue(row, col + 1, tail)
+            q.Enqueue(row, col - 1, tail)
+    found
+
+let part1 inputPath dim take  =
+    // Create a region with a boundary of solid #.
+    let topBottom = String.replicate (dim + 2) "#"
+    let middle = "#" + (String.replicate dim ".") + "#"
+    let region =
+        {0..dim-1}
+        |> Seq.fold (fun lines _ -> middle :: lines) [topBottom]
+        |> List.append [topBottom]
+        |> array2D
+
+    // Mark the bytes falling into the region.
+    let byteDrops =
+        File.ReadAllLines inputPath
+        |> Array.map (fun line -> line.Split(',') |> Array.map (int >> ((+) 1)))
+    byteDrops
+        |> Array.take take
+        |> Array.iter (fun [|col; row|] -> region[row, col] <- '#')
+
+    List.length (findPath region) - 1 |> printfn "part1: %d"
+
+    byteDrops
+    |> Array.skip take
+    |> Array.pick (fun [|col; row|] ->
+        region |>
+            Array2D.iteri (fun irow icol c ->
+                if c = ',' then
+                    region[irow, icol] <- '.'
+            )
+        region[row, col] <- '#'
+        if findPath region = [] then Some (col - 1, row - 1)
+        else None)
+    |> printfn "part2: %A"
+
+[<EntryPoint>]
+let main argv =
+    part1 "sample1.txt" 7 12
+    part1 "input.txt" 71 1024
+
+    0
