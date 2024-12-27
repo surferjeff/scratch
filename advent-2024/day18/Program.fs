bf4b5eece96d6a3c39db851dc24c0ca93310b2b7
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-27 13:48:54 -0800

    Find the shortest path.

--- advent-2024/day18/Program.fs
+++ advent-2024/day18/Program.fs
@@ -1,4 +1,5 @@
 ﻿open System.IO
+open System.Collections.Generic
 
 [<EntryPoint>]
 let main argv =
@@ -11,15 +12,37 @@ let main argv =
         |> Seq.fold (fun lines _ -> middle :: lines) [topBottom]
         |> List.append [topBottom]
         |> array2D
+
     // Mark the bytes falling into the region.
     File.ReadAllLines argv[0]
         |> Seq.take 12
         |> Seq.map (fun line -> line.Split(',') |> Array.map int)
         |> Seq.iter (fun [|col; row|] -> region[row + 1, col + 1] <- '#')
+
+    // Find the path with bread-first search.
+    let q = Queue([1, 1, []])
+    let mutable found = []
+    while q.Count > 0 do
+        let (row, col, tail) = q.Dequeue()
+        if row = dim && col = dim then
+            found <- (row, col) :: tail
+            q.Clear()
+        elif region[row, col] = '.' then
+            region[row, col] <- ','
+            let tail = (row, col) :: tail
+            q.Enqueue(row + 1, col, tail)
+            q.Enqueue(row - 1, col, tail)
+            q.Enqueue(row, col + 1, tail)
+            q.Enqueue(row, col - 1, tail)
+
+    // Update the region with the path.
+    found |> List.iter (fun (row, col) -> region[row, col] <- 'O')
     region
         |> Array2D.iteri (fun row col c ->
             match col with
             | col when col - 1 = dim -> printfn "%c" c
             | col -> printf "%c" c
         )
+    List.length found - 1 |> printfn "%d"
+
     0
