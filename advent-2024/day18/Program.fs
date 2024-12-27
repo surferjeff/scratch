5987999191a5bee6a0ee3cd2777344bcd2568d94
Jeffrey Rennie <surferjeff@gmail.com>
2024-12-27 12:23:31 -0800

    Print the region.

--- advent-2024/day18/Program.fs
+++ advent-2024/day18/Program.fs
@@ -1,13 +1,25 @@
-﻿
+﻿open System.IO
+
 [<EntryPoint>]
 let main argv =
-    let dim = 6
-    let topBottom = String.replicate (dim + 3) "#"
-    let middle = "#" + (String.replicate (dim + 1) ".") + "#"
+    // Create a region with a boundary of solid #.
+    let dim = 7
+    let topBottom = String.replicate (dim + 2) "#"
+    let middle = "#" + (String.replicate dim ".") + "#"
     let region =
-        {0..dim}
+        {0..dim-1}
         |> Seq.fold (fun lines _ -> middle :: lines) [topBottom]
         |> List.append [topBottom]
-        |> List.toArray
-        |> Array.iter (printfn "%s")
+        |> array2D
+    // Mark the bytes falling into the region.
+    File.ReadAllLines argv[0]
+        |> Seq.take 12
+        |> Seq.map (fun line -> line.Split(',') |> Array.map int)
+        |> Seq.iter (fun [|col; row|] -> region[row + 1, col + 1] <- '#')
+    region
+        |> Array2D.iteri (fun row col c ->
+            match col with
+            | col when col - 1 = dim -> printfn "%c" c
+            | col -> printf "%c" c
+        )
     0
