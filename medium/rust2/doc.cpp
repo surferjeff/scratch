04f6e51313d89dc85d77f17eaa104148a598e3d6
Jeffrey Rennie <jeff@rennieglen.com>
2025-07-21 13:33:51 -0700

    doc.cpp

---
+++ medium/rust2/doc.cpp
@@ -0,0 +1,38 @@
+class Doc {
+    // Stores the lookup table copied from the end of the file.
+    hash_map<unsigned, size_t> lookup_table;
+    // Stores objects recently loaded from the file.
+    hash_map<unsigned, Object*> object_cache;
+    // Pointer to the open file.
+    FILE* f;
+    // Counts incoming pointers.  This object gets deleted when ref_count
+    // drops to zero.
+    int ref_count;
+};
+
+class Object {
+    // Pointer to the document that owns this object.
+    Doc* doc; 
+    // Counts incoming pointers.  This object gets deleted when ref_count
+    // drops to zero.
+    int ref_count; 
+    // Writes the object to a PDF file.   
+    virtual void write_to_file(FILE* f) = 0;
+};
+
+class String : Object {
+    string s;
+    void write_to_file(FILE* f);
+};
+
+class Array : Object {
+    vector<Object*> array;
+    void write_to_file(FILE* f);
+};
+
+class Link : Object {
+    unsigned object_number;
+    void write_to_file(FILE* f);
+};
+
+// ...
