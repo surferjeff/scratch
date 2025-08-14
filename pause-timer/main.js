da5993a13eb543ef18266851b51d293b405a9474
Jeffrey Rennie <surferjeff@gmail.com>
2025-08-14 00:07:39 -0700

    Pause ABN AM 1710 audio stream.

--- pause-timer/main.js
+++ pause-timer/main.js
@@ -1,9 +1,22 @@
 (function() {
     let mins = Number(prompt("Pause in how many minutes?"));
+    let pause = () => {
+        /* For SiriusXM */
+        let pauseButton = document.querySelector('button[aria-label="Pause"]');
+        if (pauseButton) {
+            pauseButton.click();
+            return;
+        }
+        /* For AM 1710 */
+        let abn = document.getElementById("abnaudio");
+        if (abn) {
+            abn.pause();
+            return;
+        }
+        throw new Error("Failed to find pause button or audio stream");
+    };
     if (mins > 0) {
-        let timeout = window.setTimeout(
-            () => document.querySelector('button[aria-label="Pause"]').click(),
-            mins * 60 * 1000);
+        let timeout = window.setTimeout(pause, mins * 60 * 1000);
         console.log("Set timeout", timeout);
     }
 }())
